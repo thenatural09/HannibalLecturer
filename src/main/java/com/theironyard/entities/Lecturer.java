16f62a6b51af107f929461f04f03be0f24cfe833
Troy <tward4@tulane.edu>
2016-10-30 14:12:33 -0400

    Adding resources

--- src/main/java/com/theironyard/entities/Lecturer.java
+++ src/main/java/com/theironyard/entities/Lecturer.java
@@ -28,4 +28,39 @@ public class Lecturer {
         this.topic = topic;
         this.image = image;
     }
+
+    public Lecturer() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
 }
